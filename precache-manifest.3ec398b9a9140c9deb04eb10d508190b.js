9062f2521334fc6c20112947daab4ce818faba08
Oleg <salahovol@ya.ru>
2019-07-09 16:27:05 +0600

    Updates

---
+++ precache-manifest.3ec398b9a9140c9deb04eb10d508190b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20c14e98540c90900047",
+    "url": "/salahov30/salahov30.github.io/calendar-react/static/js/runtime~main.20c14e98.js"
+  },
+  {
+    "revision": "5be06bb306d342912347",
+    "url": "/salahov30/salahov30.github.io/calendar-react/static/js/main.5be06bb3.chunk.js"
+  },
+  {
+    "revision": "8af1d5efd96522105252",
+    "url": "/salahov30/salahov30.github.io/calendar-react/static/js/2.8af1d5ef.chunk.js"
+  },
+  {
+    "revision": "5be06bb306d342912347",
+    "url": "/salahov30/salahov30.github.io/calendar-react/static/css/main.f74f1824.chunk.css"
+  },
+  {
+    "revision": "1958f99259acfbbf6a1f9ac094904c5d",
+    "url": "/salahov30/salahov30.github.io/calendar-react/index.html"
+  }
+];
